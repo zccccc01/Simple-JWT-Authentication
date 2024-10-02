a1a8a50af05fcf7efc9c97682c0478a91277bfd5
cailanzz <121802791+zccccc01@users.noreply.github.com>
2024-10-02 17:05:31 +0800

    Update connection.go

--- go-auth/database/connection.go
+++ go-auth/database/connection.go
@@ -10,7 +10,8 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "root:123456@/world?charset=utf8&parseTime=True&loc=Local"
+	// 将username改为自己的MySQL用户名,passwd改为自己的密码,yourdb改为你使用的库
+	dsn := "username:passwd@/yourdb?charset=utf8&parseTime=True&loc=Local"
 	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("could not connect to the database")
