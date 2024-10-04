4f90e71a535500fc4a2cdd6753b035bdf1c9593f
cailanzz <121802791+zccccc01@users.noreply.github.com>
2024-10-04 19:43:13 +0800

    Delete go-auth directory

--- go-auth/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	ID       int    `json:"id" gorm:"primaryKey"` // 确保 ID 是主键
-	Tel      string `json:"tel" gorm:"unique"`    // 电话号码唯一
-	Password string `json:"password"`             
-	Name     string `json:"name"`
-}
