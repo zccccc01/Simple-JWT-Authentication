a71ca50c64deed6bcc96c6d90ef91912515c9d81
cailanzz <121802791+zccccc01@users.noreply.github.com>
2024-10-04 19:46:37 +0800

    Update user.go

--- go-auth/models/user.go
+++ go-auth/models/user.go
@@ -3,6 +3,6 @@ package models
 type User struct {
 	ID       int    `json:"id" gorm:"primaryKey"` // 确保 ID 是主键
 	Tel      string `json:"tel" gorm:"unique"`    // 电话号码唯一
-	Password string `json:"password"`             // 可以打个-标签忽略掉,不返回,但是忽略了post传不进去
+	Password string `json:"password"`             
 	Name     string `json:"name"`
 }
